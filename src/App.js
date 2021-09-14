5d07709953a595468274d5011eeb8074a2186d86
unknown <danielmainawaweru@gmail.com>
2021-09-14 18:58:58 +0300

    Add calculator board

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
+      <Calculator />
     </div>
   );
 }
