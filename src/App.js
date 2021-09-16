12752bc2ae98fce1e391794b1c0d0de6120f1d9a
unknown <danielmainawaweru@gmail.com>
2021-09-16 11:48:30 +0300

    Refactor calculator and app classes to function components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App (){
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,69 +1,79 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState } from 'react';
 import calculate from '../logic/Calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.handleCalculation = this.handleCalculation.bind(this);
-  }
 
-  handleCalculation(e) {
-    const obj = calculate(this.state, e.target.value);
-    this.setState(obj);
-  }
+function Calculator(){
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-  render() {
-    const { total, next, operation } = this.state;
+  const handleCalculation = e =>{
+    setState({ ...state,...calculate(state, e.target.value) });
+  }
+  
     return (
       <div className="container">
         <div className="card">
           <div className="card-body">
-            <div className="text">
-              {total}
-              {' '}
-              {operation}
-              {' '}
-              {next}
+            <div className="text">{state.total} {state.operation} {state.next}</div>
+            <div className="row">
+              <input className="background col-3" onClick={handleCalculation} type="button" value="AC" />
+              <input className="background col-3" onClick={handleCalculation} type="button" value="+/-" />
+              <input className="background col-3" onClick={handleCalculation} type="submit" value="%" />
+              <input className="btn  danger col-3"onClick={handleCalculation} type="reset" value="÷" />
             </div>
             <div className="row">
-              <input className="background col-3" onClick={this.handleCalculation} type="button" value="AC" />
-              <input className="background col-3" onClick={this.handleCalculation} type="button" value="+/-" />
-              <input className="background col-3" onClick={this.handleCalculation} type="submit" value="%" />
-              <input className="btn  danger col-3" onClick={this.handleCalculation} type="reset" value="÷" />
+              <input className="background col-3" onClick={handleCalculation} type="button" value="7" />
+              <input className="background col-3" onClick={handleCalculation} type="button" value="8" />
+              <input className="background col-3" onClick={handleCalculation} type="submit" value="9" />
+              <input className="btn danger col-3" onClick={handleCalculation} type="reset" value="x" />
             </div>
             <div className="row">
-              <input className="background col-3" onClick={this.handleCalculation} type="button" value="7" />
-              <input className="background col-3" onClick={this.handleCalculation} type="button" value="8" />
-              <input className="background col-3" onClick={this.handleCalculation} type="submit" value="9" />
-              <input className="btn danger col-3" onClick={this.handleCalculation} type="reset" value="x" />
+              <input className="background col-3" onClick={handleCalculation} type="button" value="4" />
+              <input className="background col-3" onClick={handleCalculation} type="button" value="5" />
+              <input className="background col-3" onClick={handleCalculation} type="submit" value="6" />
+              <input className="btn danger col-3" onClick={handleCalculation} type="reset" value="-" />
             </div>
             <div className="row">
-              <input className="background col-3" onClick={this.handleCalculation} type="button" value="4" />
-              <input className="background col-3" onClick={this.handleCalculation} type="button" value="5" />
-              <input className="background col-3" onClick={this.handleCalculation} type="submit" value="6" />
-              <input className="btn danger col-3" onClick={this.handleCalculation} type="reset" value="-" />
+              <input className="background col-3" onClick={handleCalculation} type="button" value="1" />
+              <input className="background col-3" onClick={handleCalculation} type="button" value="2" />
+              <input className="background col-3" onClick={handleCalculation} type="submit" value="3" />
+              <input className="btn danger col-3" onClick={handleCalculation} type="reset" value="+" />
             </div>
             <div className="row">
-              <input className="background col-3" onClick={this.handleCalculation} type="button" value="1" />
-              <input className="background col-3" onClick={this.handleCalculation} type="button" value="2" />
-              <input className="background col-3" onClick={this.handleCalculation} type="submit" value="3" />
-              <input className="btn danger col-3" onClick={this.handleCalculation} type="reset" value="+" />
-            </div>
-            <div className="row">
-              <input className="background col-6" onClick={this.handleCalculation} type="submit" value="0" />
-              <input className="background col-3" onClick={this.handleCalculation} type="submit" value="." />
-              <input className="btn danger col-3" onClick={this.handleCalculation} type="submit" value="=" />
+              <input className="background col-6" onClick={handleCalculation} type="submit" value='0'/>
+              <input className="background col-3" onClick={handleCalculation} type="submit" value="." />
+              <input className="btn danger col-3" onClick={handleCalculation} type="submit" value="=" />
             </div>
           </div>
         </div>
       </div>
     );
-  }
+
+
 }
+// class Calculator extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//         total: 0,
+//         next: null,
+//         operation: null,
+//       };
+//       handleCalculation = handleCalculation.bind(this);
+//   }
+
+//   handleCalculation(e) {
+//     const obj = calculate(this.state, e.target.value);
+//     this.setState(obj);
+//   }
+
+//   render() {
+    
+//   }
+// }
 
 export default Calculator;
