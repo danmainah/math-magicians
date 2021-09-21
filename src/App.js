66d4ca6d79eb927c1c5aa5af946689c9e8f3fd1f
unknown <danielmainawaweru@gmail.com>
2021-09-21 17:40:02 +0300

    Add quote and home files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Calculator from './components/Calculator';
 
 const App = () => (
